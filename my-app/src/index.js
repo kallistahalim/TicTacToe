53577946f3d27a49dc425a5afab6e2527ef175cd
kallistahalim <kallistahalim@gmail.com>
2020-02-19 13:33:09 -0800

    simple tic tac toe done with simple design

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 function Square(props) {
         return (
-            <button className = "square" onClick={props.onClick}>
+            <button type="button" className="btn btn-light" onClick={props.onClick}>
             {props.value}
             </button>
         );
@@ -71,6 +71,15 @@ class Board extends React.Component {
 };
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true,
+        };
+    }
     render() {
         return (
             <div className="game">
